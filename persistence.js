e8b227c0f1fcf18baa42d62f3998189f6af9ccea
LordBigglesworth <javanpohl@gmail.com>
2021-06-23 17:08:21 -0600

    created the multiplicativePersistence function

--- persistence.js
+++ persistence.js
@@ -1,8 +1,19 @@
-function persistence (n) {
+function additivePersistence (num, count = 0) {
 	// the additive persistence of an integer is the number of times you have to replace n with the sum of its digits until n becomes a single digit
-	if (n.toString().length === 1) {
-		return 0
+	if (num.toString().length === 1) {
+		return count 
 	}
+	count += 1
+	let val = num.toString().split('').reduce((accumulator, currentValue) => accumulator + parseInt(currentValue), 0)
+	return additivePersistence(val, count)
 }
-
-module.exports = persistence
+function multiplicativePersistence (num, count = 0) {
+	// the multiplicative persistence of an integer is the number of times you have to replace n with the product of its digits until n becomes a single digit
+	if (num.toString().length === 1) {
+		return count 
+	}
+	count += 1
+	let val = num.toString().split('').reduce((accumulator, currentValue) => accumulator * parseInt(currentValue), 1)
+	return multiplicativePersistence(val, count)
+}
+module.exports =  { additivePersistence, multiplicativePersistence }
--- persistence.test.js
+++ persistence.test.js
@@ -1,5 +1,17 @@
-const persistence = require('./persistence')
+const { additivePersistence, multiplicativePersistence } = require("./persistence");
 
-test('should return 0 when it receives a single digit number', () => {
-	expect(persistence(9)).toBe(0)
+test("additivePersistence should return 0 when it receives a single digit number", () => {
+  expect(additivePersistence(9)).toBe(0);
+});
+
+test("additivePersistence should return 3 when it receives 1679583", () => {
+  expect(additivePersistence(1679583)).toBe(3);
+})
+
+test("multiplicativePersistence should return 0 when it receives a single digit number", () => {
+  expect(multiplicativePersistence(9)).toBe(0);
+});
+
+test("multiplicativePersistence should return 4 when it receives 77", () => {
+  expect(multiplicativePersistence(77)).toBe(4);
 })
