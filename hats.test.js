e16118f7ee36357391eaac36a01ac0dcf0726d8c
LordBigglesworth <javanpohl@gmail.com>
2021-06-24 12:27:14 -0600

    added two other tests for hats, including one that will result in 'None' being returned

--- hats.test.js
+++ hats.test.js
@@ -2,12 +2,24 @@ const hats = require('./hats')
 
 let testArray = [5,[1,1,1,1,1]]  
 let testResult = 1
-test(`expects that hats(${testArray}) will return 1`, () => {
+test(`expects that hats(${testArray}) will return ${testResult}`, () => {
 	expect(hats(testArray)).toBe(testResult)
 })
 
 testArray = [3, [23, 11, 19, 9, 36]]
 testResult = 18
-test(`expects that hats(${testArray}) will return 1`, () => {
+test(`expects that hats(${testArray}) will return ${testResult}`, () => {
+	expect(hats(testArray)).toBe(testResult)
+})
+
+testArray = [650, [23, 11, 19, 9, 36]]
+testResult = 2001
+test(`expects that hats(${testArray}) will return ${testResult}`, () => {
+	expect(hats(testArray)).toBe(testResult)
+})
+
+testArray = [9, [23, 11, 19, 9, 36]]
+testResult = 'None'
+test(`expects that hats(${testArray}) will return ${testResult}`, () => {
 	expect(hats(testArray)).toBe(testResult)
 })
