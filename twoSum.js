2ddbd1cd33694a1834531c9e413ee9332bc37e6d
LordBigglesworth <javanpohl@gmail.com>
2021-07-26 10:44:21 -0600

    changed function so that it wouldn't use the same index/element twice

--- twoSum.js
+++ twoSum.js
@@ -5,6 +5,9 @@ const twoSum = function(nums, target) {
 	// negative numbers are possible
 	for (let i = 0; i < nums.length; i += 1) {
 		for (let j = 0; j < nums.length; j += 1) {
+			if (i === j) {
+				continue
+			}
 			if (nums[i] + nums[j] === target) {
 				return [i,j]
 			}
